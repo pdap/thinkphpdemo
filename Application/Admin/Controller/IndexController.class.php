e10ea5d58b45da2a5449f113a187c67e3cf46128
pdap <1150992600@qq.com>
2015-05-04 19:52:16 +0800

    前端布局

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show("dsfaf");
+       $this->display();
     }
 
 }
